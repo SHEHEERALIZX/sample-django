115723d04a52bb65932414abc3841c5f57c85d0b
SHEHEERALIZX <nssheheerali@gmail.com>
2020-08-13 03:14:02 +0530

    post method added

--- calc/urls.py
+++ calc/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('add/', views.add, name ='add'),
+    path('add', views.add, name ='add'),
     path('contact/', views.contact)
 ]
